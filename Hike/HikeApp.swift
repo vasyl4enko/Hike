185a8d0a4f3ea83fa69563a58f46fbcee3586ed5
Oleksandr <oleksandr@MacBook-Pro-Oleksandr.local>
2023-06-29 18:39:31 +0300

    init

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Oleksandr on 29.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
